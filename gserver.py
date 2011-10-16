4d7bc415aced5de34cdb770ac045a596bc42c863
Job Vranish <job.vranish@gmail.com>
2011-10-16 15:53:01 -0400

    Basic game server

--- gserver.py
+++ gserver.py
@@ -1,19 +1,123 @@
 import pyhnet.hnet as hnet
+import sys
 
+def threadSafe(f):
+    def g(self, *args, **kwargs):
+        with self.lock:
+            return f(self, *args, **kwargs)
+    return g
+
+class GameError(Exception): pass
+class PlayerAlreadyAdded(GameError): pass
+class PlayerAlreadyInGame(GameError): pass
+
+#TODO need to make Ids classes, with a del function and 
+    # make it work with a special with statement somehow
+class Game:
+    def __init__(self, gameName):
+        self.players = {}
+        self.clients = {}
+        self.gameName = gameName
+        self.playerIds = hnet.IdGen()
+        self.lock = hnet.RLock()
+        self.frame = hnet.Counter(0)
+        self.gameFinished = hnet.Event()
+        self.gameFinished.set()
+    
+    @threadSafe
+    def startGame(self):
+        if self.gameFinished.isSet():
+            self.gameFinished.clear()
+            hnet.startNewThread(self.run)
+            print "Game started:", self.gameName
+        
+    def run(self):
+        while not self.gameFinished.isSet():
+          self.frame()
+          self.sendGamePacket(("NewFrame", None), playerId = -1)
+          self.gameFinished.wait(0.016667)
+            
+    def stop(self):
+        self.gameFinished.set()
+        print "Game ended:", self.gameName
+    
+    def sendGamePacket(self, packet, playerId = None):
+        playerId = self.clients[hnet.getClientId()]
+        with self.frame.lock: # we don't want the server to send out the frame end packet before another packet in that frame
+            for clientId, handler, _ in self.players.values():
+                handler.send((playerId, self.frame.value, packet))
+    
+    @threadSafe
+    def newPlayer(self, playerData):
+        for clientId, _, _ in self.players.values():
+            if clientId == hnet.getClientId():
+                raise PlayerAlreadyAdded("You are already a player in that game") # make custom exception for this 
+        playerId = self.playerIds.nextId()
+        self.players[playerId] = (hnet.getClientId(), hnet.getHandler(), playerData)
+        self.clients[hnet.getClientId()] = playerId
+        hnet.getHandler().runOnClose.append(lambda : self.delPlayer(playerId, hnet.getClientId()))
+        self.startGame()
+        self.sendGamePacket(("PlayerJoined", playerData))
+        return playerId
+        
+    def delPlayer(self, playerId, clientId):
+        del self.players[playerId]
+        del self.clients[clientId]
+        self.playerIds.releaseId(playerId)
+        if not self.players:
+            self.stop()
+  
+class GameManager:
+  def __init__(self, server):
+      self.server = server
+      self.games = {}
+      self.lock = hnet.Lock()
+      self.clients = {}
+      #TODO keep track of clients to make sure they only join one game
+   
+  @threadSafe
+  def joinGame(self, gameName, playerData):
+      clientId = hnet.getClientId()
+      if clientId in self.clients:
+        raise PlayerAlreadyInGame(self.clients[clientId][0])
+      if gameName not in self.games:
+          self.games[gameName] = Game(gameName)
+      playerId = self.games[gameName].newPlayer(playerData)
+      self.clients[hnet.getClientId()] = (gameName, playerId)
+      hnet.getHandler().runOnClose.append(lambda : self.exitGame(playerData))
+      print "Player %s joined game: %s:" % (playerData, gameName) 
+      return playerId
+      
+  @threadSafe
+  def exitGame(self, playerData):
+      clientId = hnet.getClientId()
+      gameName, playerId = self.clients[clientId]
+      print "Player %s left game: %s:" % (playerData, gameName)
+      del self.clients[clientId]
+      if not self.games[gameName].players:
+        del self.games[gameName]
+      
+  def sendGamePacket(self, gameName, packet):
+      return self.games[gameName].sendGamePacket(packet)
+      
 class Server(hnet.HNetTCPServer):
     def onInit(self):
-        pass
+        self.gameManager = GameManager(self)
         
 class ServerHandler(hnet.HNetHandler):
+    def onRun(self):
+        print "connected"
+      
     def onRecv(self, packet):
         if packet.msg() == 'Hello':
-            packet.reply('Good day sir!')
+            packet.replyWithProxy(self.server.gameManager)
         
     def onInit(self):
-        pass
+        self.runOnClose = []
         
     def onClose(self):
-        pass
+        for f in self.runOnClose:
+            f()
 
 def startServer():
     with Server([('', 30131)], ServerHandler) as s:
--- testclient.py
+++ testclient.py
@@ -2,14 +2,18 @@ import pyhnet.hnet as hnet
 
 class MyClientHandler(hnet.HNetHandler):
     def runConnection(self):
-        reply = self.socket.sendAndWait('Hello')
-        print reply.msg()
+        reply = self.sendAndWait('Hello')
+        obj = reply.proxy()
+        playerId = obj.joinGame("MyGame", "Job")
+        obj.sendGamePacket("MyGame", "asdf")
+        obj.sendGamePacket("MyGame", "asdfasdgf")
+        #time.sleep(0.3)
         # NOTE! we disconnect when this function returns
         #  if you want to for the remote side to disconnect, you can do:
-        #  self.done.wait()
+        self.done.wait(0.3)
         
     def onRecv(self, packet):
-        pass
+        print packet.msg()
         
 if __name__ == "__main__":
     MyClientHandler(hnet.connectTCP('localhost', 30131)).run().wait(1.0)
